93b654f6d7bc2f2210f746873e0143192255f8eb
Kristina <krismarstina@gmail.com>
2022-09-14 14:27:21 +0300

    done

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,6 +2,9 @@ const BASE_URL = 'https://restcountries.com/v3.1';
 function fetchCountries(inputValue) {
   const url = `${BASE_URL}/name/${inputValue}?fields=name,capital,population,flags,languages`;
   return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
     return response.json();
   });
 }
